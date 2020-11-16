eb14d8782fd7dfe0b3b81f2ade8be2bb905a7a42
Julian Carelli <julianignaciocarelli@gmail.com>
2020-11-15 23:05:05 -0300

    Updates

---
+++ precache-manifest.56647356c6ef75456f8df9ccd586c213.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e5af8764342dce655df610b7832dc1",
+    "url": "/Project-Store/index.html"
+  },
+  {
+    "revision": "9f297207ebb46c1ff4f4",
+    "url": "/Project-Store/static/css/2.11c8abde.chunk.css"
+  },
+  {
+    "revision": "fb48fac735bd1569c160",
+    "url": "/Project-Store/static/css/main.d390370e.chunk.css"
+  },
+  {
+    "revision": "9f297207ebb46c1ff4f4",
+    "url": "/Project-Store/static/js/2.3fe31b2a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Project-Store/static/js/2.3fe31b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb48fac735bd1569c160",
+    "url": "/Project-Store/static/js/main.230eb021.chunk.js"
+  },
+  {
+    "revision": "e82e2e5f315b24863868",
+    "url": "/Project-Store/static/js/runtime-main.dbf7b046.js"
+  },
+  {
+    "revision": "34183c4aa7989b3f21348a9ee19ec8b1",
+    "url": "/Project-Store/static/media/coat1.34183c4a.jpg"
+  },
+  {
+    "revision": "f99d60803f2721eab3b34c5c043bc646",
+    "url": "/Project-Store/static/media/coat2.f99d6080.jpg"
+  },
+  {
+    "revision": "671c72b56932d63e2ef5ee18f3367286",
+    "url": "/Project-Store/static/media/coat3.671c72b5.jpg"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/Project-Store/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/Project-Store/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/Project-Store/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/Project-Store/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/Project-Store/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/Project-Store/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/Project-Store/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/Project-Store/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/Project-Store/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/Project-Store/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/Project-Store/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/Project-Store/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/Project-Store/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/Project-Store/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/Project-Store/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "d76f4a5a61e3c826f0ad5b9508b0cc00",
+    "url": "/Project-Store/static/media/logo.d76f4a5a.png"
+  },
+  {
+    "revision": "edd7c33ccacd62e21d56e955f37a6230",
+    "url": "/Project-Store/static/media/ropa1.edd7c33c.jpg"
+  },
+  {
+    "revision": "29db671284818f0f0300b67ec3798bf5",
+    "url": "/Project-Store/static/media/ropa2.29db6712.jpg"
+  },
+  {
+    "revision": "6cc6d947957e55f5f3dec5ca4cfa0961",
+    "url": "/Project-Store/static/media/ropa3.6cc6d947.jpg"
+  },
+  {
+    "revision": "4012f9efe4049c133f2349ecc1115a5b",
+    "url": "/Project-Store/static/media/ropa4.4012f9ef.jpg"
+  },
+  {
+    "revision": "2db2fe3f7ecb5da83a4044b2796d84c7",
+    "url": "/Project-Store/static/media/ropa5.2db2fe3f.jpg"
+  },
+  {
+    "revision": "659fc18e2855b3963d31299f95cc4bce",
+    "url": "/Project-Store/static/media/shoes2.659fc18e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-store/precache-manifest.bb9e3da412288210de2c4fc607fa0890.js"
+  "/Project-Store/precache-manifest.56647356c6ef75456f8df9ccd586c213.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-store/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Project-Store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
